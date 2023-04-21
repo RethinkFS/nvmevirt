8e886d350adbbca5c55ecfe108947554920fadb9
Sang-Hoon Kim <sanghoonkim@ajou.ac.kr>
2023-04-21 16:20:15 +0900

    Change to GPL v3 and add LICENSE file

--- bitmap.c
+++ bitmap.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2020-2023
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
+ * it under the terms of the GNU General Public License version 3 as
  * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
